8a8047cce3887c7f75a0833e1a0ab7c55f5bed8a
matt <matt@fullsuite.ph>
2024-11-17 14:01:16 +0800

    ADD: Under Construction

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,11 +3,12 @@ import { NavLink } from 'react-router-dom'
 const Navbar = () => {
   return (
     <header clasName="header">
-        <NavLink to="/" className="w-10 h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md">
-            <p className="blue-gradient_text">MW</p>
-        </NavLink>
-
         <nav className="flex text-lg gap-7 font-medium">
+            <NavLink to="/" className="w-10 h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md">
+                <p className="blue-gradient_text">MW</p>
+            </NavLink>
+        
+        {/* <nav className="flex text-lg gap-7 font-medium"> */}
             <NavLink to="/about" className={({ isActive }) => isActive ? 'text-blue-500' : 'text-black'}> 
                 About
             </NavLink>
